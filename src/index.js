fb45afeb175181f740a0c2e6429e9e860d63caff
dev.pavan.km <dev.pavan.km@gmail.com>
2024-04-13 20:03:01 +0530

    feat: imports, exports & webpack modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert()
+import { AlertService } from "./app/alert.service";
+import { run } from "./app/app";
+import { ComponentService } from "./app/component.service";
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
