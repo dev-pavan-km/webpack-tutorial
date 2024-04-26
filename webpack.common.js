af65d0998ff2c1c9759576b48d4221f55529cef8
dev.pavan.km <dev.pavan.km@gmail.com>
2024-04-26 19:28:45 +0530

    feat: add prod and dev configs

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ],
+  },
+};
