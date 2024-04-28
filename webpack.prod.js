e773d923b9cfbd14f9286ceeff2470146335a2c6
dev.pavan.km <dev.pavan.km@gmail.com>
2024-04-28 12:09:56 +0530

    feat: Minify HTML, CSS, and JS in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssEtractPlugin = require("mini-css-extract-plugin");
+// const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -9,5 +11,20 @@ module.exports = merge(common, {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  // optimization: {
+  //   minimizer: [new OptimizeCssAssetsPlugin()],
+  // },
+  plugins: [new MiniCssEtractPlugin({ filename: "[name].[contenthash].css" }), new CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssEtractPlugin.loader, //3. Extract css into files
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });
